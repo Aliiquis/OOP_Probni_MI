509787e9e5795ba9e586bc49eba476ddb37a11e7
Aliiquis <127763335+Aliiquis@users.noreply.github.com>
2023-04-24 17:59:07 +0200

    Delete exercise1/hr/fer/oop/zad1 directory

--- exercise1/hr/fer/oop/zad1/Status.java
+++
@@ -1,5 +0,0 @@
-package hr.fer.oop.zad1;
-
-public enum Status {
-    FUNCTIONAL, NEED_CHECK, OUT_OF_SERVICE
-}
--- exercise1/hr/fer/oop/zad1/Unmanned.java
+++
@@ -1,5 +0,0 @@
-package hr.fer.oop.zad1;
-
-public interface Unmanned {
-	double coverArea(Number height);
-}
