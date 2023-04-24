23cee170faa85cee1fde65105af730517e8dde93
Aliiquis <127763335+Aliiquis@users.noreply.github.com>
2023-04-24 17:59:24 +0200

    Delete exercise3/hr/fer/oop/zad3 directory

--- exercise3/hr/fer/oop/zad3/Printable.java
+++
@@ -1,5 +0,0 @@
-package hr.fer.oop.zad3;
-
-public interface Printable {
-    String print();
-}
--- exercise3/hr/fer/oop/zad3/PrintableList.java
+++
@@ -1,4 +0,0 @@
-package hr.fer.oop.zad3;
-
-public abstract class PrintableList<T> extends MyList<T> implements Printable {
-}
