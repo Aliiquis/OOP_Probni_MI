e25640ef66a9e9f8b7423be7a2e209d34a015ccb
Aliiquis <127763335+Aliiquis@users.noreply.github.com>
2023-04-25 20:05:41 +0200

    Update Parser.java

--- probni_MI_2023/exercise4/hr/fer/oop/Parser.java
+++ probni_MI_2023/exercise4/hr/fer/oop/Parser.java
@@ -7,51 +7,37 @@ import java.util.Locale;
 public class Parser {
 
     public static Reading parseInputString(String inputReadingString) throws ParseReadingException {
+        String[] args = inputReadingString.split(", ");
+        if (args.length < 3) throw new IncorrectReadingFormatException();
+
         Integer deviceId;
         LocalDate timestamp;
-        StringBuilder input = new StringBuilder(inputReadingString);
-        int numOfCommas = 0;
-        for (int i = 0; i < input.length() - 1; i++) {
-            if (input.charAt(i) == ',' && input.charAt(i + 1) == ' ') {
-                numOfCommas++;
-            }
-        }
-        SensorReadingValue[] sensorReadings = new SensorReadingValue[numOfCommas - 1];
-        if (numOfCommas < 2) {
-            throw new IncorrectReadingFormatException();
-        } else {
-            try {
-                deviceId = Integer.parseInt(input.substring(9, input.indexOf(",")));
-                input.delete(0, input.indexOf(" ") + 1);
-                timestamp = Parser.parseDate(input.substring(10, input.indexOf(",")));
-                input.delete(0, input.indexOf(" ") + 1);
-            } catch (Exception e) {
-                e.printStackTrace();
-                throw new FormatException(e);
-            }
+        SensorReadingValue[] sensorReadings = new SensorReadingValue[args.length - 2];
 
-            for (int i = 0; i < numOfCommas - 1; i++) {
-                try {
-                    String attributeName = input.substring(0, input.indexOf(":"));
-                    String attributeValue;
-                    if (i == numOfCommas - 2) {
-                        attributeValue = input.substring(input.indexOf(":") + 1, input.length());
-                    } else {
-                        attributeValue = input.substring(input.indexOf(":") + 1, input.indexOf(","));
-                    }
-                    sensorReadings[i] = new SensorReadingValue(attributeName, attributeValue);
-                } catch (Exception e) {
-                    sensorReadings[i] = null;
-                } finally {
-                    input.delete(0, input.indexOf(" ") + 1);
-                }
-            }
-            boolean f = true;
-            for (SensorReadingValue s : sensorReadings) {
-                if (s != null) f = false;
-            }
-            if (f) throw new ReadingsFormatException();
+        try {
+            deviceId = Integer.parseInt(args[0].substring(args[0].indexOf(":") + 1));
+            timestamp = Parser.parseDate(args[1].substring(args[1].indexOf(":") + 1));
+        } catch (Exception e) {
+            throw new FormatException(e);
         }
+
+        for (int i = 0; i < sensorReadings.length; i++) {
+            try {
+                String readingName = args[i + 2].substring(0, args[i + 2].indexOf(":"));
+                String readingValue = args[i + 2].substring(args[i + 2].indexOf(":") + 1);
+                sensorReadings[i] = new SensorReadingValue(readingName, readingValue);
+            } catch (Exception e) {
+                sensorReadings[i] = null;
+            }
+        }
+        boolean notNull = false;
+        for (SensorReadingValue readingValue : sensorReadings) {
+            if (readingValue != null) {
+                notNull = true;
+                break;
+            }
+        }
+        if (!notNull) throw new ReadingsFormatException();
         return new Reading(deviceId, sensorReadings, timestamp);
     }
 
@@ -59,8 +45,6 @@ public class Parser {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM-dd-yyyy");
         dtf = dtf.withLocale(Locale.GERMAN);
         LocalDate date = LocalDate.parse(dateA, dtf);
-
         return date;
     }
-
 }
